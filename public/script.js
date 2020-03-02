ecf91c54ec4041968e8d5ff23b1faf09dd6cce6d
Ji, Jessie <asaggo@byui.edu>
2020-03-01 20:53:25 -0800

    adduser function added

--- public/script.js
+++ public/script.js
@@ -2,12 +2,26 @@ const add_user_button = document.getElementById("add_user");
 
 let data = [];
 
-// fetch random uers
+// get a random uer
 async function getUser() {
   const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
-
   console.log(data.results[0]);
+
+  const new_user_obj = {
+    first_name: data.results[0].name.first,
+    last_name: data.results[0].name.last,
+    money: Math.floor(Math.random() * 100000)
+  };
+  addUser(new_user_obj);
 }
+
+// add a user
+function addUser(user_info) {
+  data.push(user_info.name);
+}
+
+//update DOM
+function updateDOM(updatedData = data) {}
 
 getUser();
