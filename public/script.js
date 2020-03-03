17bdc7740c2db6dbdf8ede45b561f7d396841477
Jungwon Oh <jungwonoh@Jungwons-MacBook-Pro.local>
2020-03-02 21:26:38 -0800

    add a user, double money, and sort by richest added

--- public/script.js
+++ public/script.js
@@ -1,12 +1,15 @@
 const add_user_button = document.getElementById("add_user");
-
+const double_money_button = document.getElementById("double_money");
+const main = document.getElementById("main");
+const sort = document.getElementById("sort");
+const mill = document.getElementById("mill");
 let data = [];
 
 // get a random uer
 async function getUser() {
   const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
-  console.log(data.results[0]);
+  // console.log(data.results[0]);
 
   const new_user_obj = {
     first_name: data.results[0].name.first,
@@ -17,11 +20,50 @@ async function getUser() {
 }
 
 // add a user
-function addUser(user_info) {
-  data.push(user_info.name);
+function addUser(new_user_obj) {
+  data.push(new_user_obj);
+  updateDOM();
 }
 
 //update DOM
-function updateDOM(updatedData = data) {}
+function updateDOM(updatedData = data) {
+  main.innerHTML = "";
 
-getUser();
+  updatedData.forEach(item => {
+    const row_element = document.createElement("div");
+    row_element.classList.add("flex", "w-full", "justify-between");
+
+    row_element.innerHTML = `<strong>${item.first_name} ${
+      item.last_name
+    }</strong>
+    <p>${formatMoney(item.money)}</p>
+    `;
+
+    main.appendChild(row_element);
+  });
+}
+
+// number to dollar format
+function formatMoney(number) {
+  return `$${number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")}`;
+}
+
+function doubleMoney() {
+  data = data.map(user => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
+}
+
+function sortByRichest() {
+  data.sort((a, b) => {
+    return b.money - a.money;
+  });
+
+  updateDOM();
+}
+
+add_user_button.addEventListener("click", getUser);
+double_money_button.addEventListener("click", doubleMoney);
+sort.addEventListener("click", sortByRichest);
+mill.addEventListener();
